2eb68601b380e2ccde7335b9c1615eaebcddd568
Flávio Arêas <vflavio9@gmail.com>
2022-11-13 22:14:10 -0300

    feat: create text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,25 @@
+import { clsx } from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
+import { ReactNode } from 'react';
+
+export interface TextProps {
+  children: ReactNode;
+  size?: 'sm' | 'md' | 'lg';
+  asChild?: boolean;
+}
+
+export function Text({ children, asChild = false, size = 'md' }: TextProps) {
+  const Comp = asChild ? Slot : 'span';
+
+  return (
+    <Comp
+      className={clsx('text-gray-100', {
+        'text-xs': size === 'sm',
+        'text-sm': size === 'md',
+        'text-md': size === 'lg',
+      })}
+    >
+      {children}
+    </Comp>
+  );
+}
